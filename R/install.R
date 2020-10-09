bf846a661c08b0ca95a5d30474a3df07b9301df4
Freddy Drennan <fdrennan@Freddys-MacBook-Pro-2.local>
2020-10-08 23:45:31 -0600

    update

--- R/install.R
+++ R/install.R
@@ -11,6 +11,32 @@
 #' }
 
 # https://github.com/ropensci/git2r.git
+#' install_workflower
+#' @export install_workflower
+install_workflower <- function(delete = FALSE) {
+  workflower_home <- fs::path_expand("~/.workflower")
+  cli::cli_alert_info("Setting up HOME directory paths for workflower at {workflower_home}")
+
+  if (delete) {
+    if (fs::dir_exists(workflower_home)) {
+      fs::dir_delete(workflower_home)
+    }
+  }
+
+  if (fs::dir_exists(workflower_home)) {
+    print(fs::dir_ls(workflower_home, all = TRUE))
+    cli::cli_alert_info(readr::read_file(file.path(workflower_home, "installation", "gitssh.txt")))
+    cli::cli_alert_warning("workflower already exists at {workflower_home}\nSkipped for now")
+    return(FALSE)
+  }
+
+  fs::dir_create(workflower_home)
+  cmd_git <- glue::glue("git clone https://github.com/fdrennan/workflower.git {workflower_home}")
+  cli::cli_alert_info(cmd_git)
+  system(cmd_git)
+  cli::cli_alert_success("Installed workflower at {workflower_home}")
+}
+
 
 #' install_workflower
 #' @export install_workflower
@@ -53,3 +79,5 @@ update_package <- function(params = NULL) {
   renv::snapshot(prompt = FALSE)
   devtools::install()
 }
+
+
