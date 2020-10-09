0dd5f017c5f576a959cf8bac8bedd51612127da2
Freddy Drennan <fdrennan@Freddys-MacBook-Pro-2.local>
2020-10-09 08:49:44 -0600

    update

--- .workflower/installation/install.R
+++ .workflower/installation/install.R
@@ -1,3 +1,6 @@
 renv::install('fdrennan/workflower', prompt = FALSE)
 devtools::install(wf_home())
 renv::restore(prompt = FALSE)
+
+# path <- file.path(workflower::wf_home(), '.workflower/installation/install.R');
+# path <- glue::glue('Rscript {path}')
